df5f08177e24b1e871edc0486607b7ca5720ff2d
evost <konstantin.kraynov@yandex.ru>
2017-11-26 22:17:08 +0300

    Added Discord login, bot.ready, guildMemberAdd functions

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // © Copyright 2017 Konstantin Kraynov
 // Contacts: <konstatin.kraynov@yandex.ru> <https://github.com/evost>
 // All rights reserved
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+var prefix = '!';
+
+bot.on('ready', () => {
+    console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.find('name', 'member-log');
+    if (!channel) return;
+    channel.send(`Welcome to the guild, ${member}`);
+});
+
+bot.login(Buffer.from('TXpnME16YzJNemcxTXpRd05EUXdOVGMyLkRQeUhsdy55dGxYYklzeEJrUjYwUHg5VXo3cTJZQVd1RG8=', 'base64').toString());
